60936955f915609d780c6d911092c2deafc51ef3
Adam Chalmers <adam.s.chalmers@gmail.com>
2015-09-18 09:01:27 +1000

    Docstrings.

--- cli.py
+++ cli.py
@@ -3,28 +3,37 @@ import click
 
 @click.group()
 def cli():
+  """Mando is a tool for finding mandolin chords."""
   pass
 
 @cli.command()
 @click.argument("note1")
 @click.argument("note2")
 def dist(note1, note2):
+  """Outputs the number of semitones between note1 and note2."""
   click.echo(music.distance(note1, note2))
 
 @cli.command()
 @click.argument("note")
 def note(note):
+  """Outputs (string, fret) pairs which produce the given note."""
   positions = music.fingers_for_note(note)
-  out = ", ".join([note + str(pos) for note, pos in positions])
+  out = "\n".join([note + str(pos) for note, pos in positions])
   click.echo(out)
 
 @cli.command()
+@click.option("--verbose", is_flag=True, help="Prints extra information.")
 @click.argument("chord")
 @click.argument("n", default=5)
-def chord(chord, n):
-  click.echo("The chord %s has notes %s" % (chord, ", ".join(music.notes_in_chord(chord))))
+def chord(verbose, chord, n):
+  """Outputs (G, D, A, E) fingerings for the given chord."""
+  if verbose:
+    click.echo("The chord %s has notes %s" % (chord, ", ".join(music.notes_in_chord(chord))))
   for fingering, notes in music.fingers_for_chord(chord, n):
-    click.echo ("%s plays %s" % (fingering, ", ".join(notes)))
+    if verbose:
+      click.echo ("%s plays %s" % (fingering, ", ".join(notes)))
+    else:
+      click.echo(" ".join([str(x) for x in fingering]))
 
 if __name__ == "__main__":
   cli()
